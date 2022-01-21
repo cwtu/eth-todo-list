78ea1cf8cec140d2a76bf1304c6271a0660fc2e5
Adam <kcbs96115@gmail.com>
2022-01-21 16:29:54 -0500

    List tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,5 +1,24 @@
 pragma solidity >=0.5.0;
 
 contract TodoList {
+	// state variable
 	uint public taskCount = 0;
+
+	// struct type
+	struct Task {
+		uint id;
+		string content;
+		bool completed;
+	}
+
+	mapping(uint => Task) public tasks;
+
+	constructor() public {
+		createTask("Default Task");
+	}
+
+	function createTask(string memory _content) public {
+		taskCount ++;
+		tasks[taskCount] = Task(taskCount, _content, false);
+	}
 }
