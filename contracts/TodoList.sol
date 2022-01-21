9f252262d0aba0d96b380dba293d93f9c579b731
Adam <kcbs96115@gmail.com>
2022-01-21 16:43:32 -0500

    create tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -13,6 +13,13 @@ contract TodoList {
 
 	mapping(uint => Task) public tasks;
 
+
+	event TaskCreated(
+		uint id,
+		string content,
+		bool completed
+	);
+
 	constructor() public {
 		createTask("Default Task");
 	}
@@ -20,5 +27,6 @@ contract TodoList {
 	function createTask(string memory _content) public {
 		taskCount ++;
 		tasks[taskCount] = Task(taskCount, _content, false);
+		emit TaskCreated(taskCount, _content, false);
 	}
 }
